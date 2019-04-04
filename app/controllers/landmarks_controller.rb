6799a6ade8023751dfc4cc10dbaf376eb62dfffa
PrincetonJRose <decoyzpr@gmail.com>
2019-04-04 15:19:42 -0400

    Worked on landmarks models

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,25 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  # set :views, ‘./app/views/landmarks’
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/edit"
+  end
+
+  patch '/landmarks/:id' do
+    Landmark.update(params[:id], name: params[:name], year_completed: params[:year_completed].to_i)
+    redirect "/landmarks/#{params[:id]}"
+  end
+
 end
